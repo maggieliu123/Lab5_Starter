8d530e555bfaa1e6830d35737c47b10624182822
Maggie Liu <maggieliu.252@gmail.com>
2022-10-30 12:38:17 -0700

    can select horn and set correct img and audio, can play sound but not correct volume

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,62 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    selectHorn();
 }
+
+/* listens for the event of selecting a horn */
+function selectHorn() {
+    // get select component
+    var selectOptions = document.getElementById("horn-select");
+
+    // get image
+    var imageLst = document.querySelectorAll('img');
+    var hornImg = imageLst[0];
+
+    // get audio
+    var audio = document.querySelector('audio');
+
+    // get play button
+    var playButton = document.querySelector('button');
+
+    // listen for option to be clicked, then set horn img and audio
+    selectOptions.addEventListener('click', setHorn);
+    playButton.addEventListener('click', playSound);
+
+    /* sets the audio and image for horn */
+    function setHorn() {
+        // airhorn settings for selected horn
+        var selectedHorn = selectOptions.value;
+
+        if (selectedHorn == "air-horn") {
+            hornImg.src = "assets/images/air-horn.svg";                    
+            audio.src = "assets/audio/air-horn.mp3";
+        }
+
+        else if (selectedHorn == "car-horn") {
+            hornImg.src = "assets/images/car-horn.svg";                    
+            audio.src = "assets/audio/car-horn.mp3";
+        }
+
+        else if (selectedHorn == "party-horn") {
+            hornImg.src = "assets/images/party-horn.svg";                    
+            audio.src = "assets/audio/party-horn.mp3";
+        }
+    }
+
+    /* plays audio of horn */
+    function playSound() {
+        audio.play();
+    }
+}
+
+
+
+
+
+
+function test() {
+    console.log("test");
+}
+
+
